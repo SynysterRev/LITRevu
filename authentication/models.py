a5446261eb636d3bd871b756fae77624662839be
Michael <56340359+SynysterRev@users.noreply.github.com>
2025-01-13 13:09:12 +0100

    create review app and correct models

--- authentication/models.py
+++ authentication/models.py
@@ -1,37 +1,21 @@
-from django.contrib.auth.models import AbstractUser
-from django.core.validators import MinValueValidator, MaxValueValidator
 from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models import ForeignKey
-
-
-class Ticket(models.Model):
-    # Your Ticket model definition goes here
-    pass
-
-
-class Review(models.Model):
-    ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
-    rating = models.PositiveSmallIntegerField(
-        # validates that rating must be between 0 and 5
-        validators=[MinValueValidator(0), MaxValueValidator(5)])
-    headline = models.CharField(max_length=128)
-    body = models.CharField(max_length=8192, blank=True)
-    user = models.ForeignKey(
-        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    time_created = models.DateTimeField(auto_now_add=True)
 
 
 class UserFollows(models.Model):
     # Your UserFollows model definition goes here
     user = ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='following')
     followed_user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='followed_by')
+
+
     class Meta:
         # ensures we don't get multiple UserFollows instances
         # for unique user-user_followed pairs
-        unique_together = ('user', 'followed_user', )
+        unique_together = ('user', 'followed_user',)
+
 
 class User(AbstractUser):
-
     profile_picture = models.ImageField(default='default_profile.png', upload_to='profile_pictures/')
     followed = models.ManyToManyField(settings.AUTH_USER_MODEL, through=UserFollows)
---
+++ review/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from review.models import *
+
+admin.site.register(Ticket)
+admin.site.register(Review)
